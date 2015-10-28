147eec729828edb0ed981ec561be73303e925b48
Arthur Berman <aberman@resnet160-242.medford.tufts.edu>
2015-10-28 19:40:56 -0400

    Added pnm tests #4
    It is important to maintain good unit tests for behavior. This test
    does not regress, but does execute the behavior.

---
+++ src/pnm_tests.cpp
@@ -0,0 +1,10 @@
+#include "pnm_image.h"
+#include <iostream>
+using namespace std;
+int main() {
+    PnmImage pnm(100, 100, 255);
+    for (int x = 0; x < 100; x++) {
+        pnm.set_pixel(x, x, pixel_t(255, 255, 255));
+    }
+    pnm.write(stdout);
+}
