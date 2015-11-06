3b777cf318144343091c31534777b3a7ca9d0c22
Max Bernstein <tekknolagi@gmail.com>
2015-11-06 10:46:11 -0500

    Return point from intersect_ray

--- src/ray_tracer.h
+++ src/ray_tracer.h
@@ -4,6 +4,7 @@
 #include <inttypes.h>
 #include <cmath>
 #include <iostream>
+#include <algorithm>
 
 #include "pnm_image.h"
 #include "vector.h"
@@ -18,26 +19,41 @@ struct sphere_t {
         this->radius = radius;
     }
 
-    /* I learned how to check if a ray intersects a circle from here:
-       http://www.purplealienplanet.com/node/20 */
-    bool intersect_ray(ray_t ray) {
+    /*
+      I learned how to check if a ray intersects a circle from here:
+      http://www.purplealienplanet.com/node/20. If the ray and the sphere do not
+      intersect, intersection_point is left untouched. If they do intersect, it
+      is changed to the closest intersection point to the camera.
+    */
+
+    bool intersect_ray(ray_t ray, vector3_t &intersection_point) {
         float A = ray.direction.dot(ray.direction);
         vector3_t dist = ray.start - loc;
         float B = 2 * ray.direction.dot(dist);
         float C = dist.dot(dist) - radius * radius;
         float disc = B * B - 4 * A * C;
-        return disc >= 0;
+        if (disc < 0) {
+            return false;
+        }
+        else {
+            float dist1 = -B + sqrt(B * B - 4 * A * C)/(2 * A);
+            float dist2 = -B - sqrt(B * B - 4 * A * C)/(2 * A);
+            float min_distance = std::min(dist1, dist2);
+            intersection_point = ray.start * min_distance;
+
+            return true;
+        }
     }
 };
 
 class RayTracer {
-    public:
+public:
     RayTracer(sphere_t *scene, size_t num_objects);
     ~RayTracer();
     PnmImage render_image(size_t width, size_t height);
     pixel_t cast_ray(ray_t r);
 
-    private:
+private:
     sphere_t *scene;
     size_t num_objects;
 };
