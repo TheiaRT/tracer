eef1ec8d5f3979443c00dc9f7b877517abc34067
Max Bernstein <tekknolagi@gmail.com>
2015-11-06 13:34:21 -0500

    Fix style on SceneObject

--- src/scene_object.h
+++ src/scene_object.h
@@ -17,8 +17,7 @@ struct material_t {
 };
 
 /* Virtual Object Class. All 3d objects in our scene inherit from this class. */
-class SceneObject
-{
+class SceneObject {
   public:
     virtual bool intersect_ray(ray_t ray, vector3_t &point) = 0;
 
