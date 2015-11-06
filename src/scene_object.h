d49a3ba99663d05df974ff1b48e4b606c07fc7c5
Thomas Colgrove <trcolgrove@gmail.com>
2015-11-06 13:22:26 -0500

    Add virtual SceneObject class.
    All objects in the scene will inherit from this class.

---
+++ src/scene_object.h
@@ -0,0 +1,24 @@
+#include "pnm_image.h"
+#include "vector.h"
+
+struct color_t {
+    double r, g, b, a;
+}
+
+struct material_t {
+    color_t ambient;
+    color_t diffuse;
+    color_t specular;
+    color_t emission;
+    double shine;
+}
+
+/* Virtual Object Class. All 3d objects in our scene inherit from this class. */
+class SceneObject
+{
+public:
+    virtual bool intersect_ray(ray_t ray, vector3_t &point) = 0;
+
+protected:
+    material_t material;
+}
