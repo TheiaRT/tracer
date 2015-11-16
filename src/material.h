329347f6e319a66a52e14378e0de2f64d64d2411
Max Bernstein <tekknolagi@gmail.com>
2015-11-16 18:52:54 -0500

    Add constructors for material_t

--- src/material.h
+++ src/material.h
@@ -50,34 +50,62 @@ struct material_t {
     color_t specular;
     color_t emission;
     double shine;
+
+    material_t() :
+        ambient(color_t()),
+        diffuse(color_t()),
+        specular(color_t()),
+        emission(color_t()),
+        shine(0.0f)
+    {
+
+    }
+
+    material_t(color_t ambient,
+               color_t diffuse,
+               color_t specular,
+               color_t emission,
+               double shine) :
+        ambient(ambient),
+        diffuse(diffuse),
+        specular(specular),
+        emission(emission),
+        shine(shine)
+    {
+    }
 };
 
 /* http://www.nicoptere.net/dump/materials.html */
-const material_t BRASS = {
-    .ambient  = color_t(0.329412, 0.223529, 0.027451, 1),
-    .diffuse  = color_t(0.780392, 0.568627, 0.113725, 1),
-    .specular = color_t(0.992157, 0.941176, 0.807843, 1),
-    .shine    = 27.8974
-}, BRONZE = {
-    .ambient  = color_t(0.2125, 0.1275, 0.054, 1),
-    .diffuse  = color_t(0.714, 0.4284, 0.18144, 1),
-    .specular = color_t(0.393548, 0.271906, 0.166721, 1),
-    .shine    = 25.6
-}, CHROME = {
-    .ambient  = color_t(0.25, 0.25, 0.25, 1),
-    .diffuse  = color_t(0.4, 0.4, 0.4, 1),
-    .specular = color_t(0.774597, 0.774597, 0.774597, 1),
-    .shine    = 76.8
-}, JADE = {
-    .ambient  = color_t(0.135, 0.2225, 0.1575, 0.95),
-    .diffuse  = color_t(0.54, 0.89, 0.63, 0.95),
-    .specular = color_t(0.316228, 0.316228, 0.316228, 0.95),
-    .shine    = 12.8
-}, PEARL = {
-    .ambient  = color_t(0.25, 0.20725, 0.20725, 0.922),
-    .diffuse  = color_t(1, 0.829, 0.829, 0.922),
-    .specular = color_t(0.296648, 0.296648, 0.296648, 0.922),
-    .shine    = 11.264
-};
+const material_t BRASS = material_t(
+    color_t(0.329412, 0.223529, 0.027451, 1),
+    color_t(0.780392, 0.568627, 0.113725, 1),
+    color_t(0.992157, 0.941176, 0.807843, 1),
+    color_t(),
+    27.8974
+), BRONZE = material_t(
+    color_t(0.2125, 0.1275, 0.054, 1),
+    color_t(0.714, 0.4284, 0.18144, 1),
+    color_t(0.393548, 0.271906, 0.166721, 1),
+    color_t(),
+    25.6
+), CHROME = material_t(
+    color_t(0.25, 0.25, 0.25, 1),
+    color_t(0.4, 0.4, 0.4, 1),
+    color_t(0.774597, 0.774597, 0.774597, 1),
+    color_t(),
+    76.8
+), JADE = material_t(
+    color_t(0.135, 0.2225, 0.1575, 0.95),
+    color_t(0.54, 0.89, 0.63, 0.95),
+    color_t(0.316228, 0.316228, 0.316228, 0.95),
+    color_t(),
+    12.8
+), PEARL = material_t(
+    color_t(0.25, 0.20725, 0.20725, 0.922),
+    color_t(1, 0.829, 0.829, 0.922),
+    color_t(0.296648, 0.296648, 0.296648, 0.922),
+    color_t(),
+    11.264
+);
 
 #endif
